0c4bc5231880720ce6f32e234d8508facc9a91aa
Kerija E. Baumane <kerija.baumane@gmail.com>
2023-09-17 00:51:16 +0300

    This is a small project for anime lovers using if-elif-else nested statements. 😍

---
+++ replit-challenge-1.py
@@ -0,0 +1,22 @@
+print("This one is for anime lovers!")
+print()
+anime = input("What is your favorite anime? ")
+
+if anime == "Demon Slayer" or anime == "demon slayer":
+  print("WOW, great!")
+  faveCharacter = input("Who is your favorite character? ")
+  if faveCharacter == "Nezuko":
+    print("Right answer")
+  else:
+    print("Nah, Nezuko's the greatest")
+    
+elif anime == "One Piece" or anime == "one piece":
+  print("Aww, that's nice.")
+  onePeace = input("🏴‍☠️  What is the one piece? ")
+  if onePeace == "friends":
+   print("Right answer")
+  else:
+   print("Wrong answer! It's the friends we make along the way. (👉ﾟヮﾟ)👉")
+    
+else:
+  print("Yeah, I like this one too.")
